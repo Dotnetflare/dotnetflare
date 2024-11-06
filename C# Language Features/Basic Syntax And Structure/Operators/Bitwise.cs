2de6d788fed8af5885565f17859e9c3a7cae8118
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-11-06 19:07:49 +0330

    RemoveComments

--- C# Language Features/Basic Syntax And Structure/Operators/Bitwise.cs
+++ C# Language Features/Basic Syntax And Structure/Operators/Bitwise.cs
@@ -6,27 +6,21 @@ public class Bitwise
     {
         int result;
 
-        // Bitwise AND Operator
         result = a & b;
         Console.WriteLine("Bitwise AND: " + result);
 
-        // Bitwise OR Operator
         result = a | b;
         Console.WriteLine("Bitwise OR: " + result);
 
-        // Bitwise aOR Operator
         result = a ^ b;
         Console.WriteLine("Bitwise aOR: " + result);
 
-        // Bitwise Complement Operator
         result = ~a;
         Console.WriteLine("Bitwise Complement: " + result);
 
-        // Bitwise LEFT SHIFT Operator
         result = a << 2;
         Console.WriteLine("Bitwise Left Shift: " + result);
 
-        // Bitwise RIGHT SHIFT Operator
         result = a >> 2;
         Console.WriteLine("Bitwise Right Shift: " + result);
     }
--- C# Language Features/Basic Syntax And Structure/Operators/Relational.cs
+++ C# Language Features/Basic Syntax And Structure/Operators/Relational.cs
@@ -6,29 +6,21 @@ public class Relational
     {
         bool validate;
 
-        //Greater than operator: (denoted by “>”): Validates greater than the relation between operands.
         validate = a > b;
         Console.WriteLine($"a > b: {validate}");
 
-        //Less than operator: (denoted by “<“): Validates less than the relation between operands.
         validate = a < b;
         Console.WriteLine($"a < b: {validate}");
 
-        //Equals To operator: (denoted by “==”): Validates the equality of two operands.
         validate = a == b;
         Console.WriteLine($"a == b: {validate}");
 
-        //Greater than or equals to (denoted by “>=”):
-        //Validates greater than or equals to the relation between the two operands.
         validate = a >= b;
         Console.WriteLine($"a >= b: {validate}");
 
-        //Less than or equals to (denoted by “<=”):
-        //Validates less than or equals to the relations between the two operands.
         validate = a <= b;
         Console.WriteLine($"a <= b: {validate}");
 
-        //Not equal: (denoted by “!=”): Validates not an equal relationship between the two operands.
         validate = a != b;
         Console.WriteLine($"a != b: {validate}\n");
     }

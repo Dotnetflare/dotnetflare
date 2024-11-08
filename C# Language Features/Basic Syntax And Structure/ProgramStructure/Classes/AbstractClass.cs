0f365a140979dc5138baa2b39517e45bf96856c1
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-11-08 20:02:06 +0330

    AddAbstractMethod

---
+++ C# Language Features/Basic Syntax And Structure/ProgramStructure/Classes/AbstractClass.cs
@@ -0,0 +1,34 @@
+﻿namespace ProgramStructure.Classes;
+
+// Keywrod: abstract
+public abstract class AbstractClass
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    protected AbstractClass(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+
+    // Abstract method to be implemented by derived classes
+    public abstract string GetInfo();
+}
+
+public class DerivedFromAbstractClass : AbstractClass
+{
+    public string Name { get; set; }
+
+    public DerivedFromAbstractClass(int id, string title, string name)
+        : base(id, title)
+    {
+        Name = name;
+    }
+
+    // Override method to provide specific info
+    public override string GetInfo()
+    {
+        return $"{Id}: {Title} by {Name}.";
+    }
+}

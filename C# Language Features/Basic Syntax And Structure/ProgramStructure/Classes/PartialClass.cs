f12b9845869eb8fc1cea47be482ca2e1d64df9b0
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-11-08 20:56:54 +0330

    CompleteClasses

---
+++ C# Language Features/Basic Syntax And Structure/ProgramStructure/Classes/PartialClass.cs
@@ -0,0 +1,20 @@
+﻿namespace ProgramStructure.Classes;
+
+// Keyword: partial
+public partial class PartialClass
+{
+    private List<string> items = [];
+
+    public void AddItem(string item)
+    {
+        items.Add(item);
+    }
+}
+
+public partial class PartialClass
+{
+    public void DisplayItems()
+    {
+        Console.WriteLine("PartialClass_2 Display Items");
+    }
+}
